7973080a4252e15b3769c5be8cd3ded2ca7a2995
ackroydAI <ackdroid3@gmail.com>
2019-10-20 13:16:01 +0100

    commit

--- no-sudo.sh
+++ no-sudo.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#check dependency requirement
+
+
+if ! [[ /bin/sed ]]; then
+	echo " you need to install sed "
+	exit 1
+fi
+
+#then execute script
+
 sed -i -e 's/sudo//g' path_to_yourfile.sh
